8275e568d7b0f80b1adf1dc6ac70110c1dbcf5db
WillQi <williamqipizza@gmail.com>
2021-05-02 00:03:40 -0600

    MemoryDataSource complete and adds new memory datasource for numbers

--- core/src/main/java/io/github/willqi/leaderboardapi/core/datasources/MemoryDataSource.java
+++ core/src/main/java/io/github/willqi/leaderboardapi/core/datasources/MemoryDataSource.java
@@ -1,50 +1,62 @@
 package io.github.willqi.leaderboardapi.core.datasources;
 
-import io.github.willqi.leaderboardapi.core.datasources.exceptions.DataSourceException;
-
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * This DataSource does not use any database connection and it's sole purpose
  * is to provide a datasource for those who do not want to use any sort of database
+ * Note: add cannot be used with this DataSource because there is no guarantee that V is a number.
  * @param <K> Type of the identifier the value is associated with
  * @param <V> Values must implement the Comparable interface and will be sorted using the compareTo method
  */
 public class MemoryDataSource<K, V extends Comparable<V>> implements DataSource<K, V> {
 
-    @Override
-    public List<Record<K, V>> getTop(int placings) throws DataSourceException {
-        return null;
+    protected final Map<K, V> scores;
+
+    public MemoryDataSource() {
+        this.scores = new ConcurrentHashMap<>();
     }
 
     @Override
-    public V get(K key) throws DataSourceException {
-        return null;
+    public List<Record<K, V>> getTop(int placings) {
+        return this.scores.entrySet()
+                .stream()
+                .sorted((entryA, entryB) -> entryB.getValue().compareTo(entryA.getValue()))
+                .map(entry -> new Record<>(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void set(K key, V value) throws DataSourceException {
-
+    public V get(K key) {
+        return this.scores.getOrDefault(key, null);
     }
 
     @Override
-    public void add(K key, V value) throws DataSourceException {
-
+    public void set(K key, V value) {
+        this.scores.put(key, value);
     }
 
     @Override
-    public void remove(K key) throws DataSourceException {
-
+    public void add(K key, V value) {
+        throw new UnsupportedOperationException("MemoryDataSource does not support add.");
     }
 
     @Override
-    public void reset() throws DataSourceException {
+    public void remove(K key) {
+        this.scores.remove(key);
+    }
 
+    @Override
+    public void reset() {
+        this.scores.clear();
     }
 
     @Override
     public void close() {
-
+        this.scores.clear();
     }
 
 }
---
+++ core/src/main/java/io/github/willqi/leaderboardapi/core/datasources/MemoryNumericalDoubleDataSource.java
@@ -0,0 +1,22 @@
+package io.github.willqi.leaderboardapi.core.datasources;
+
+/**
+ * This DataSource does not use any database connection and it's sole purpose
+ * is to provide a datasource for those who do not want to use any sort of database
+ * BUT are using numerical values.
+ * @param <K> Type of the identifier the value is associated with
+ */
+public class MemoryNumericalDoubleDataSource<K> extends MemoryDataSource<K, Double> {
+
+    @Override
+    public void add(K key, Double value) {
+        this.scores.compute(key, (k, currentValue) -> {
+           if (currentValue == null) {
+               return value;
+           } else {
+               return currentValue + value;
+           }
+        });
+    }
+
+}
